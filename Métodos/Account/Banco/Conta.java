16de822cdf2a3e4d09033cc223985e43091e328b
Higor Campos <56975692+higorcamposs@users.noreply.github.com>
2023-02-24 10:31:37 -0300

    Update Conta.java

--- Métodos/Account/Banco/Conta.java
+++ Métodos/Account/Banco/Conta.java
@@ -3,6 +3,14 @@ public class Conta {
     Double saldo,lim;
     Pessoa tit;
     
+    Conta(String n, double s, double l, Pessoa t){
+        this.numero = n;
+        this.saldo = s;
+        this.lim = l;
+        this.tit = t;
+        System.out.println("Nova conta criada!");
+    }
+    
     Double disponivel(){
         return this.saldo + this.lim;
     }
@@ -12,26 +20,26 @@ public class Conta {
     }
     void depositar(double v){
         this.saldo += v;
-        System.out.println("✔ Depositar realizado!");
+        System.out.println("âœ” Depositar realizado!");
     }
     boolean sacar(double v){
         if (v <= this.disponivel()){
             this.saldo -= v;
-            System.out.println("✔ Saque realizado!");
+            System.out.println("âœ” Saque realizado!");
             return true;
         }
         else{
-            System.out.println("❌ Sem saldo!");
+            System.out.println("âŒ Sem saldo!");
             return false;
         }
     }
     void transferir(double v, Conta dest){
         if(this.sacar(v)){
             dest.depositar(v);
-            System.out.println("✔ Transferência realizada!");
+            System.out.println("âœ” TransferÃªncia realizada!");
         }
         else{
-            System.out.println("❌ Transferir não realizado!");
+            System.out.println("âŒ Transferir nÃ£o realizado!");
         }
     }
       
