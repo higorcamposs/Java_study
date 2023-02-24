69b4fd7f5eb9b322ce9a4a494cf7fa110d386af1
Higor Campos <56975692+higorcamposs@users.noreply.github.com>
2023-02-24 09:18:46 -0300

    Create Conta.java

---
+++ Métodos/Account/Banco/Conta.java
@@ -0,0 +1,38 @@
+public class Conta {
+    String numero;
+    Double saldo,lim;
+    Pessoa tit;
+    
+    Double disponivel(){
+        return this.saldo + this.lim;
+    }
+    void extrato(){
+        System.out.println("Titular: " + this.tit.nome);
+        System.out.println("Saldo disponivel: R$" + this.disponivel());
+    }
+    void depositar(double v){
+        this.saldo += v;
+        System.out.println("❌ Depositar realizado!");
+    }
+    boolean sacar(double v){
+        if (v <= this.disponivel()){
+            this.saldo -= v;
+            System.out.println("✔ Saque realizado!");
+            return true;
+        }
+        else{
+            System.out.println("❌ Sem saldo!");
+            return false;
+        }
+    }
+    void transferir(double v, Conta dest){
+        if(this.sacar(v)){
+            dest.depositar(v);
+            System.out.println("✔ Transferência realizada!");
+        }
+        else{
+            System.out.println("❌ Transferir não realizado!");
+        }
+    }
+      
+}
